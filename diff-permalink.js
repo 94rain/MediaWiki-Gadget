d9523a031110ce00affe971442cef75065d83e0b
94rain <94rain@msn.com>
2019-10-22 13:40:52 +0800

    +diff-permalink.js

---
+++ diff-permalink.js
@@ -0,0 +1,29 @@
+﻿// <nowiki>
+( function () {
+    $.when( $.ready, mw.loader.using( [ "mediawiki.util" ] ) ).then( function () {
+        var suffix = mw.config.get( "wgDiffNewId" );
+        var page;
+        if( suffix ) {
+            if( document.getElementsByClassName( "diff-multi" ).length ||
+	    		mw.config.get("wgPageName") === "Special:ComparePages" )
+                suffix = mw.config.get( "wgDiffOldId" ) + "/" + suffix;
+            page = "Special:Diff/" + suffix;
+        } else {
+            var oldidMatch = mw.util.getParamValue( "oldid" );
+            if( oldidMatch ) {
+                page = "Special:Permalink/" + oldidMatch;
+            } else return; // 什么也不返回
+        }
+
+        var permalinkEl = $( "<input>" ).val( page )
+            .click( function () { this.select(); document.execCommand( "copy" ); } );
+        permalinkEl.attr( "size", permalinkEl.val().length ); // resize to diff length
+        if( suffix ) {
+            $( "#bodyContent" ).prepend( permalinkEl )
+                .prepend( "此差异的固定链接： " );
+        } else {
+            $( "#contentSub" ).after( permalinkEl ).after( "此修订版本的固定链接： " );
+        }
+    } );
+} )();
+// </nowiki>
