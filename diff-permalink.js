1508c18644e2a3256296ff655f4f26913a558dfe
94rain <94rain@msn.com>
2019-10-22 21:13:01 +0800

    diff-permalink: update from upstream

---
+++ diff-permalink.js
@@ -0,0 +1,29 @@
+﻿// <nowiki>
+( function () {
+    $.when( $.ready, mw.loader.using( [ "mediawiki.util" ] ) ).then( function () {
+        var suffix = mw.config.get( "wgDiffNewId" );
+        var page;
+        if( suffix ) {
+            if( document.getElementsByClassName( "diff-multi" ).length ||
+	    		mw.config.get("wgPageName") === "Special:ComparePages" )
+                suffix = mw.config.get( "wgDiffOldId" ) + "/" + suffix;
+            page = "Special:Diff/" + suffix;
+        } else {
+            var oldidMatch = mw.util.getParamValue( "oldid" );
+            if( oldidMatch ) {
+                page = "Special:Permalink/" + oldidMatch;
+            } else return; // nothing to do here
+        }
+
+        var permalinkEl = $( "<input>" ).val( page )
+            .click( function () { this.select(); document.execCommand( "copy" ); } );
+        permalinkEl.attr( "size", permalinkEl.val().length ); // resize to diff length
+        if( suffix ) {
+            $( "#bodyContent" ).prepend( permalinkEl )
+                .prepend( "Permalink to this diff: " );
+        } else {
+            $( "#contentSub" ).after( permalinkEl ).after( "Permalink to this oldid: " );
+        }
+    } );
+} )();
+// </nowiki>
